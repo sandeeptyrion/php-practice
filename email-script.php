d11f3d9307884950f9b58fb14a7ab1317a6c993f
sandeeptyrion <86090965+sandeeptyrion@users.noreply.github.com>
2021-06-19 13:41:49 +0530

    Update email-script.php

--- email-script.php
+++ email-script.php
@@ -1,7 +1,8 @@
 <?php
-require 'PHPMailerAutoload.php';
-$mail->isSMTP();
+require '/PHPMailerAutoload.php';
+
 $mail = new PHPMailer;
+$mail->isSMTP();
                                       // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
