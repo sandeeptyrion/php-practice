23139a2606819438eb79c66e9116800927ab8a2a
sandeeptyrion <86090965+sandeeptyrion@users.noreply.github.com>
2021-06-19 09:53:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "Hello World! this is the home page";
-echo '<a href="http://https://php-practice1655.herokuapp.com/sample.php">Click here</a>';
+echo '<a href="php-practice1655.herokuapp.com/sample.php">Click here</a>';
 ?>
