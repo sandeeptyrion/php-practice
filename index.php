770efcddb5fe608141d8146e70324f3b2ef9f2a3
sandeeptyrion <86090965+sandeeptyrion@users.noreply.github.com>
2021-06-20 11:07:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'sandeepthatikonda1655@gmail.com';                 // SMTP username
-$mail->Password = 'nfkciohycepgqyns';                           // SMTP password
+$mail->Password = 'cntrgumltkiaodhws';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 $mail->setFrom('from@example.com', 'Mailer');
