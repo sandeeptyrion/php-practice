820523513e9f09d49c1b35d8c0e21e4f3ed39d48
sandeeptyrion <86090965+sandeeptyrion@users.noreply.github.com>
2021-06-20 08:24:19 +0530

    Update email-script.php

--- email-script.php
+++ email-script.php
@@ -1,15 +1,5 @@
 <?php
-require '/PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
-
-                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'sandeepthatikonda1655@gmail.com';                 // SMTP username
-$mail->Password = 'nfkciohycepgqyns';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
 if (isset($_POST['sendMailBtn'])) {
     $fromEmail = $_POST['fromEmail'];
     $toEmail = $_POST['toEmail'];
