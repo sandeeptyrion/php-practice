6174a4cd75665520293b3cab9431c342a973b939
sandeeptyrion <86090965+sandeeptyrion@users.noreply.github.com>
2021-06-20 11:17:14 +0530

    Update email-script.php

--- email-script.php
+++ email-script.php
@@ -1,9 +1,5 @@
 <?php
-require 'PHPMailerAutoload.php';
-
-$mail = new PHPMailer;
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
+                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'sandeepthatikonda1655@gmail.com';                 // SMTP username
