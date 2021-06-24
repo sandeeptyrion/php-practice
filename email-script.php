c338c4cbf15fd74d372ea9e1a27f3bd33642c1f1
sandeeptyrion <86090965+sandeeptyrion@users.noreply.github.com>
2021-06-24 15:20:45 +0530

    Update email-script.php

--- email-script.php
+++ email-script.php
@@ -11,7 +11,7 @@ $mail->SMTPDebug = 0;
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'sandeepthatikonda1655@gmail.com';                 // SMTP username
-$mail->Password = 'cntrgumltkiaodhws';                           // SMTP password
+$mail->Password = '';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
